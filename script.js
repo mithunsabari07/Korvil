abf402780e43398be8f8bd7b319fcad47e26e8a8
Mithun Sabari <mithunsabari17@gmail.com>
2026-04-20 17:56:07 +0530

    Add functions for navigation and toggle functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+function goLogin() {
+  window.location.href = "login.html";
+}
+
+function goDashboard() {
+  window.location.href = "dashboard.html";
+}
+
+function toggle() {
+  document.getElementById("container").classList.toggle("active");
+}
